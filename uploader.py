7a4b0b8981af6d3b7031178412c17872decc56af
Siddharths8212376 <siddharth8212376@gmail.com>
2020-02-19 01:04:32 +0530

    added the client side uploader for raw data

--- uploader.py
+++ uploader.py
@@ -1,4 +1,5 @@
 # the firebase uploader plugin
+# to upload the raw data constantly
 # to be run constantly using scheduler
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -8,12 +9,11 @@ import time
 cred = credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
-
+print("uploading ", end='')
 def upload_data():
     data = open('./mission_flight/new_data.txt', encoding='utf-8', errors='ignore')
     content = data.read()
-    # print(content)
-    print('added data')
+    print('.', end='')
     doc_ref = db.collection(u'raw-data').document(u'data')
     doc_ref.set({u'new_data': content})
 
@@ -22,5 +22,7 @@ schedule.every(4).seconds.do(upload_data)
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+
 
 
