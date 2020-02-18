11b118a183b92e1635eaeab99e69febf5b3efd4e
Siddharths8212376 <siddharth8212376@gmail.com>
2020-02-18 20:08:03 +0530

    updated the retriever

--- retriever.py
+++ retriever.py
@@ -8,13 +8,14 @@ cred = credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-users_ref = db.collection(u'raw-data')
+users_ref = db.collection(u'main_coordinates')
 docs = users_ref.stream()
 
 for doc in docs:
     # print(u'{} => {}'.format(doc.id, doc.to_dict()))
     data_dictionary = doc.to_dict()
     for id, data in data_dictionary.items():
-        with open(str(id) + '.txt', mode='w', encoding='utf-8', errors='ignore') as write_file:
-            write_file.write(data)
+        # with open(str(id) + '.txt', mode='w', encoding='utf-8', errors='ignore') as write_file:
+        #     write_file.write(data)
+        print(id, data)
         
