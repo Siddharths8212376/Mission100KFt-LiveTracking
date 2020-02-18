d4a593b8e8004a090d2a7e338c0c08bdcc44a981
Siddharths8212376 <siddharth8212376@gmail.com>
2020-02-19 05:26:01 +0530

    updated parser

--- parser.py
+++ parser.py
@@ -7,12 +7,17 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 
-cred = credentials.Certificate('./ServiceAccountKey.json')
+cred = credentials.Certificate('ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+# no. of documents already parsed
+global doc_count
+doc_count = 0
+# time_ = time.time()
 def parse():
-    data = open('mission_flight/raw_data.txt', encoding='utf-8', errors='ignore')
+    global doc_count
+    data = open('./mission_flight/raw_data.txt', encoding='utf-8', errors='ignore')
     def is_number(s):
         try:
             float(s)
@@ -25,17 +30,13 @@ def parse():
     coordinates = []
     index_line = 0
     for each_line in content:
-        # print(each_line)
         try:
             _, link, _ = each_line.split("'")
-            # print(link)
+
             _, location = link.split("=")
             _, coords = location.split(":")
             lat_, long_ = coords.split(",")
             
-            # print(lat_, long_)
-            # lats range from -90 to 90 
-            # and longs range from -180 to 180
             if is_number(lat_) and is_number(long_):
                 lat_ = float(lat_)
                 long_ = float(long_)
@@ -52,80 +53,37 @@ def parse():
     for index, coord in enumerate(coordinates[:-1]):
         if (abs(coord[0] - coordinates[index + 1][0]) <= 0.004 and (abs(coord[1] - coordinates[index + 1][1]) <= 0.004)):
             parse_coordinates.append(coord)
-    print(len(parse_coordinates))
+
+    coord_dict = {}
+    coord_list = []
+    for coord in parse_coordinates:
+        coord_dict["LAT"] = coord[0]
+        coord_dict["LONG"] = coord[1]
+        coord_list.append(coord_dict)
     
+
     
-    # improvements to be added
-    with open('main_coordinates.csv', mode='w') as coords_file:
-        coord_writer = csv.writer(coords_file)
-        coord_writer.writerow(['LAT', 'LONG'])
-        for coord in parse_coordinates:
-            coord_writer.writerow([coord[0], coord[1]])
-            
-    # converting the files to csv
-    csv_file = pd.DataFrame(pd.read_csv('main_coordinates.csv'))
-    csv_file.to_json('main_coords.json', orient='records', indent=4)
-    
-    # generate more points between two coordinates
-    """
-        #
-        #  intermediate points
-        # 
-        def intermediates(p1, p2, nb_points=15):
-        #Return a list of nb_points equally spaced points
-        # between p1 and p2
-        # If we have 8 intermediate points, we have 8+1=9 spaces
-        # between p1 and p2
-        x_spacing = (p2[0] - p1[0]) / (nb_points + 1)
-        y_spacing = (p2[1] - p1[1]) / (nb_points + 1)
-
-        return [[p1[0] + i * x_spacing, p1[1] +  i * y_spacing] 
-                for i in range(1, nb_points+1)]
-    set_coordinates = []"""
-    # for index, coord in enumerate(parse_coordinates[:-1]):
-    #     intermediates_ = intermediates(coord, parse_coordinates[index + 1], 20)
-    #     set_coordinates.append(coord)
-    #     for coordinate in intermediates_:
-    #         # coordinates.insert(index + 1, coordinate)
-    #         set_coordinates.append(coordinate)
-    # set_coordinates.append(coordinates[-1])
-
-    # print(len(set_coordinates))
-
-
-    # print(coordinates[:50])
-    # with open('parsed_coordinates.csv', mode='w') as coords_file:
-    #     coord_writer = csv.writer(coords_file)
-    #     coord_writer.writerow(['LAT', 'LONG'])
-    #     for coord in set_coordinates:
-    #         coord_writer.writerow([coord[0], coord[1]])
-
-    # csv to json conversion using pandas
-    # csv_file = pd.DataFrame(pd.read_csv('parsed_coordinates.csv'))
-    # csv_file.to_json('coords_new.json', orient='records', indent=4)
+    doc_ref = db.collection(u'main_coordinates')
+    for index, element in enumerate(coord_list):
+        # add if already exists check
+        if index >= doc_count:
+            element["index"] = index
+            doc = doc_ref.document(str(index))
+            doc.set(element)
+            doc_count += 1
+    print('.', end='')
 
 parse()
-
-doc_ref = db.collection(u'main_coordinates')
-with open('main_coords.json') as f:
-    data = json.load(f)
-# print(data)
-for index, element in enumerate(data):
-    # print(element)
-    # add if already exists check
-    element["index"] = index
-    # print(element)
-    doc_ref.add(element)
+now = time.time()
+# print(now - time_)
 # schedule.every(5).seconds.do(parse)
 
-# ans = 1
+
+
 # while True:
-#     # if ans == 0:
-#     #     break
+
 #     schedule.run_pending()
 #     time.sleep(1)
     
 
 
-    
-    
