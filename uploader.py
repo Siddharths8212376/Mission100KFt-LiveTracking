4e8d25c1b3e97584bebc163459a37ca9c6846765
Siddharths8212376 <siddharth8212376@gmail.com>
2020-02-19 00:38:31 +0530

    fixed the uploader. Resolved issue #6

--- uploader.py
+++ uploader.py
@@ -2,17 +2,25 @@
 # to be run constantly using scheduler
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+import schedule
+import time
 
 cred = credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-data = open('./mission_flight/lora_final.txt', encoding='utf-8', errors='ignore')
-content = data.read()
-# print(content)
+def upload_data():
+    data = open('./mission_flight/new_data.txt', encoding='utf-8', errors='ignore')
+    content = data.read()
+    # print(content)
+    print('added data')
+    doc_ref = db.collection(u'raw-data').document(u'data')
+    doc_ref.set({u'new_data': content})
 
-doc_ref = db.collection(u'raw-data').document(u'data')
-doc_ref.set({u'lora_final': content})
+schedule.every(4).seconds.do(upload_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
